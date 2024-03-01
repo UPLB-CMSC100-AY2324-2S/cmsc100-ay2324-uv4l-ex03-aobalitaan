adc80c9ab5ef84a4188ad68a515f4acae2e9cc33
Axel Balitaan <balitaanaxel@gmail.com>
2024-03-01 16:10:43 +0800

    feat: added object credentials

--- index.js
+++ index.js
@@ -1,73 +1,74 @@
-function stringEqual(str1, str2)
+function stringEqual(str1, str2)   // Function for checking if string is equal/same
 {
     return(str1 == str2);
 }
 
-function hasNumber(str)
+function hasNumber(str) // Checks if string contains a number
 {
-    for (var i = 0; i < str.length; i++)
+    for (var i = 0; i < str.length; i++)    // Iterates through every character in the string
     {
-        if (str[i].toUpperCase() == str[i].toLowerCase())
+        if (str[i].toUpperCase() == str[i].toLowerCase())   // If the character's lower and uppercase version are the same
         {
-            return true;
+            return true;    // Contains a number
         }
     }
     return false;
 }
 
-function hasLower(str)
+function hasLower(str)  // Checks if string has a lowercase character
 {
-    return (!(str == str.toUpperCase()));
+    return (!(str == str.toUpperCase()));   // Makes the whole string uppercase, if it is still the same it means that it doesn't contain a lowercase charac
 }
 
 function hasUpper(str)
 {
-    return (!(str == str.toLowerCase()));
+    return (!(str == str.toLowerCase()));   // Makes the whole string lowercase, if it is still the same it means that it doesn't contain an uppercase charac
 }
 
-function validatePassword(passw1, passw2)
+function validatePassword(passw1, passw2)   // Function for validating password
 {
-    if (!stringEqual(passw1, passw2))
+    if (!stringEqual(passw1, passw2))   // If strings are not same
     {
         console.log("Passwords don't match!");
         return false;
     }
 
-    if (passw1.length < 8)
+    if (passw1.length < 8)  // If string is not at least 8 characters
     {
         console.log("Password too short!");
         return false;
     }
 
-    if (!hasNumber(passw1))
+    if (!hasNumber(passw1)) // If string doesn't have a number
     {
         console.log("Password should have a number!");
         return false;
     }
 
-    if (!hasLower(passw1))
+    if (!hasLower(passw1))  // If string doesn't have a lowercase character
     {
         console.log("Password should have a lowercase character!");
         return false;
     }
 
-    if (!hasUpper(passw1))
+    if (!hasUpper(passw1)) // If string doesn't have an uppercase character
     {
         console.log("Password should have an uppercase character!");
         return false;
     }
     
+    //PASSWORD IS VALID
     console.log("Password is valid.")
     return true;
 }
 
 function reverseStr(str)
 {
-    var rev_str = "";
+    var rev_str = "";   // Initialize empty reverse string
 
-    for (var i = (str.length - 1); i >= 0; i--)
+    for (var i = (str.length - 1); i >= 0; i--) // Iterates from last charcter of the string
     {
-        rev_str = rev_str + str[i];
+        rev_str = rev_str + str[i]; // Concatonates each character into the rev_string
     }
 
     return rev_str;
@@ -75,11 +76,12 @@ function reverseStr(str)
 
 function storePassword(username, passw1, passw2)
 {
+    // Initializes the credentials object
     var credentials =
     {
         name: username,
         password: passw1,
-        newpassword: validatePassword(passw1, passw2) ? reverseStr(passw1) : passw1
+        newpassword: validatePassword(passw1, passw2) ? reverseStr(passw1) : passw1 // If password is valid, reverses it, else just use password 1
     }
 
     console.log(credentials);
